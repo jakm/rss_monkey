2cc62ed2650f6c4f7eb2c45452a272c1f88fef51
Jakub Matys <matys.jakub@gmail.com>
2012-10-19 10:54:59 +0200

    initial commit

---
+++ src/rss_monkey/orm.py
@@ -0,0 +1,32 @@
+# -*- coding: utf8 -*-
+
+from sqlalchemy import ForeignKey, Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class Feed(Base):
+    __tablename__ = 'feeds'
+
+    id = Column(Integer, primary_key=True)
+    url = Column(String, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    link = Column(String)
+    modified = Column(DateTime)
+    entries = relationship('FeedEntry', order_by='FeedEntry.<<datum>>',
+        backref='feed', cascade="all, delete, delete-orphan")
+
+
+class FeedEntry(Base):
+    __tablename__ = 'feed_entries'
+
+    id = Column(Integer, primary_key=True)
+    feed_id = Column(Integer, ForeignKey('feeds.id'))
+    title = Column(String)
+    summary = Column(String)
+    link = Column(String)
+    date = Column(DateTime)

19f94c74e1b1f85559a26f9e9814ead7d8808494
Jakub Matys <jakub.matys@nodus.cz>
2012-11-23 18:10:34 +0100

    Added script to clear whole database

---
+++ scripts/__clear_database.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS user_feeds;
+DROP TABLE IF EXISTS user_entries;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS feeds;
+DROP TABLE IF EXISTS feed_entries;
+
+DROP TRIGGER IF EXISTS feed_entries_after_insert_trg;

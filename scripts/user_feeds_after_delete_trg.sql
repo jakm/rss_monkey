2ee992850717e7ae44d1cd0adf9dfeed0d39bd94
Jakub Matys <jakub.matys@nodus.cz>
2012-11-24 15:10:00 +0100

    Trigger upgraded to remove feeds and entries with no reference

--- scripts/user_feeds_after_delete_trg.sql
+++ scripts/user_feeds_after_delete_trg.sql
@@ -6,8 +6,15 @@ CREATE TRIGGER user_feeds_after_delete_trg
 AFTER DELETE
 ON user_feeds
 FOR EACH ROW
+BEGIN
     DELETE FROM user_entries
     WHERE feed_id = OLD.feed_id AND user_id = OLD.user_id;
-$
+
+    -- if no user has registered this feed we can remove it
+    IF NOT EXISTS(select * from user_feeds where feed_id = OLD.feed_id) THEN
+        DELETE FROM feed_entries WHERE feed_id = OLD.feed_id;
+        DELETE FROM feeds WHERE id = OLD.feed_id;
+    END IF;
+END $
 
 delimiter ;

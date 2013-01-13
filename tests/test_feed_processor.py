915c9062edb697272e6ea358032bf88190fd6418
Jakub Matys <jakub.matys@nodus.cz>
2013-01-13 13:07:08 +0100

    Test case fixed to use adbapi

--- tests/test_feed_processor.py
+++ tests/test_feed_processor.py
@@ -1,86 +1,97 @@
 # -*- coding: utf8 -*-
 
-# uncomment for logging function calls by @log_function_call
-# import logging
-# logging.basicConfig(level=logging.INFO)
-
+import os
+import tempfile
+from twisted.enterprise import adbapi
+from twisted.internet import defer
 from twisted.trial import unittest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.pool import StaticPool
 
-from rss_monkey.common.db import DbRegistry
-from rss_monkey.common.model import Feed
+from rss_monkey.common.db import Db
 from rss_monkey.feed_processor import FeedProcessor
+
+# COMPARE WITH create_tables.sql!!!
+
+FEEDS_COMMAND = '''CREATE TABLE `feeds` (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `url` TEXT UNIQUE,
+    `title` TEXT,
+    `description` TEXT,
+    `link` TEXT,
+    `modified` TEXT
+    )'''
+
+FEED_ENTRIES_COMMAND = '''CREATE TABLE `feed_entries` (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `feed_id` INT,
+    `title` TEXT,
+    `summary` TEXT,
+    `link` TEXT,
+    `date` TEXT,
+    CONSTRAINT `feed_entries_ibfk_1` FOREIGN KEY (`feed_id`) REFERENCES `feeds` (`id`)
+    )'''
 
 
 class ProcessFeedTest(unittest.TestCase):
 
+    @defer.inlineCallbacks
     def setUp(self):
-        engine = create_engine('sqlite:///:memory:',
-                               connect_args={'check_same_thread': False},
-                               poolclass=StaticPool)
 
-        Feed.metadata.create_all(engine)
+        self.db_filename = tempfile.mktemp('.sqlite', 'rss_monkey_', '/tmp')
 
-        Session = sessionmaker(bind=engine)
+        self.db_pool = adbapi.ConnectionPool('sqlite3',
+                                             self.db_filename,
+                                             check_same_thread=False)
+        self.db = Db(self.db_pool)
 
-        self.db_registry = DbRegistry()
-        self.db_registry.session_registry = scoped_session(Session)
-        self.db = self.db_registry()
+        try:
+            yield self.db_pool.runOperation(FEEDS_COMMAND)
+            yield self.db_pool.runOperation(FEED_ENTRIES_COMMAND)
+        except Exception as e:
+            self.fail('Cannot create test database: %s' % str(e))
+
+    def tearDown(self):
+        os.unlink(self.db_filename)
 
     def get_feed_processor(self):
         processor = FeedProcessor()
-        processor.db_registry = self.db_registry
+        processor.db = self.db
         return processor
 
+    @defer.inlineCallbacks
     def test_get_one_feed(self):
-        feed = Feed()
-        feed.url = 'http://www.abclinuxu.cz/auto/zpravicky.rss'
-
-        # store test feed to mock database
-        self.db.store(feed)
-        self.db.commit()
-
-        feed_id = feed.id
+        # store feed to test database
+        url = 'http://www.abclinuxu.cz/auto/zpravicky.rss'
+        feed_id = yield self.db.add_feed(url)
 
         # test FeedProcessor
         processor = self.get_feed_processor()
-        processor.process_feed(feed_id)
+        yield processor.process_feed(feed_id)
 
         # check result
-        updated_feed = self.db.load(Feed, id=feed_id)
-        self.assertTrue(updated_feed.title, 'Feed was not updated')
+        feed = yield self.db.get_feed(feed_id)
+        self.assertTrue(feed['title'], 'Feed was not updated')
 
+    @defer.inlineCallbacks
     def test_process_feeds(self):
         # prepare test feeds
         feed_urls = ['http://www.abclinuxu.cz/auto/zpravicky.rss',
                      'http://www.root.cz/rss/zpravicky/']
         feed_ids = []
         for url in feed_urls:
-            feed = Feed()
-            feed.url = url
-            # store test feed to mock database
-            self.db.store(feed, commit=True)
-            feed_ids.append(feed.id)
+            feed_id = yield self.db.add_feed(url)
+            feed_ids.append(feed_id)
 
         # test FeedProcessor
         processor = self.get_feed_processor()
-        dl = processor.process_feeds()
+        yield processor.process_feeds()
 
-        def check_result(result):
-            for res in result:
-                if not res[0]:
-                    failure = res[1]
-                    self.fail('%s: %s' % (failure.type.__name__,
-                                          failure.getErrorMessage()))
+        # check results
+        for feed_id in feed_ids:
+            feed = yield self.db.get_feed(feed_id)
+            self.assertTrue(feed['title'],
+                            'Feed was not updated (url: %s)' % feed['url'])
 
-            for feed_id in feed_ids:
-                updated_feed = self.db.load(Feed, id=feed_id)
-                self.assertTrue(updated_feed.title,
-                    'Feed was not updated (url: %s)' % updated_feed.url)
-                self.assertGreater(len(updated_feed.entries), 0,
-                    'Entries were not stored (url: %s)' % updated_feed.url)
-
-        dl.addCallback(check_result)
-        return dl
+            res = yield self.db.get_entries(feed_id)
+            entries = tuple(res)
+            self.assertGreater(len(entries), 0,
+                              'Entries were not stored (url: %s)' % feed['url'])
